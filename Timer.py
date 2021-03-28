66b048077b3b837a1248c4ef7aaa07b880ea940c
Siddharth Gupta <81249728+Infinite-Blue-1042@users.noreply.github.com>
2021-03-28 13:58:37 -0700

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,4 +1,4 @@
-import time
+import time, sys
 a = 0
 Time = int(input("Set a timer here in seconds: "))
 
@@ -6,3 +6,4 @@ for i in range(Time, 0, -1):
     print(i)
     time.sleep(1)
 print("Your timer is complete.")
+sys.exit()
